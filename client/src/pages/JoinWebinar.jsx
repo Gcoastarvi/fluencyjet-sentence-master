740bf519f2a8ab34b27db58ecb80c9cc6d086df6
Gcoastarvi <130732365+Gcoastarvi@users.noreply.github.com>
2026-08-30 04:47:48 +0000

    Update webinar date and WhatsApp group

--- client/src/pages/JoinWebinar.jsx
+++ client/src/pages/JoinWebinar.jsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 
 const CURRENT_WEBINAR_WHATSAPP_URL =
-  "https://chat.whatsapp.com/KKZwjHYXLghIGNXiqOmkkq";
+  "https://chat.whatsapp.com/B9UhOQsN5US2R81Z6rJwD2";
 
 export default function JoinWebinar() {
   useEffect(() => {
