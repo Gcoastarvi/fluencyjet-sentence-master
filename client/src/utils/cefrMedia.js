121ce7b833c0e79ea6efb36390891b90b95593b2
Gcoastarvi <130732365+Gcoastarvi@users.noreply.github.com>
2026-09-15 05:38:42 +0000

    Add fourth CEFR listening audio asset

--- client/src/utils/cefrMedia.js
+++ client/src/utils/cefrMedia.js
@@ -4,6 +4,7 @@ const READY_AUDIO_ASSET_KEYS = new Set([
   "de-a1-d1-listening-01",
   "de-a1-d1-listening-02",
   "de-a1-d1-listening-03",
+  "de-a1-d1-listening-04",
 ]);
 
 export function isCefrAudioAssetReady(audioAssetKey) {
